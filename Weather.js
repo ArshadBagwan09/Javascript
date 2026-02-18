7c8c7e831cbc62c5e9d62053c75f429d559175d9
Arshad <bagwanarshad95@gmail.com>
2026-02-18 16:23:37 +0530

    Add files via upload
    Its Javascript program

---
+++ Weather.js
@@ -0,0 +1,12 @@
+let weather = "Sunny";
+let temperature = 35;
+
+if (weather === "Sunny") {
+  if (temperature > 30) {
+    console.log("Wear sunscreen");
+  } else {
+    console.log("Enjoy the day");
+  }
+} else if (weather === "Rainy") {
+  console.log("Take an umbrella");
+}
